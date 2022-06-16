5ba55d5161af6e068b16f66c49b843304fe11abc
Amin <amin.pe94@gmail.com>
2022-06-16 17:09:48 +0430

    Config show/hide

--- src/components/AppConfig/AppConfig.tsx
+++ src/components/AppConfig/AppConfig.tsx
@@ -1,28 +1,38 @@
+import Icon from '@components/Common/Icon';
+import useToggle from '@hooks/useToggle';
 import { useQuestions } from 'state/questions/hooks/useQuestions';
 
 const AppConfig = () => {
+  const [showConfig, toggleShowConfig] = useToggle(true);
   const { config } = useQuestions();
 
   return (
-    <div className="flex mb-10 justify-center">
-      <div
-        className="select-none cursor-pointer mr-5 last:mr-0"
-        onClick={() => {
-          config.set('enterKeyIsSend', !config.enterKeyIsSend);
-        }}
-      >
-        Enter key is send: <strong>{config.enterKeyIsSend ? 'yes' : 'no'}</strong>
-      </div>
+    <>
+      <button type="button" onClick={toggleShowConfig} className="fixed top-4 left-4">
+        <Icon name="gear" className="w-6 h-6" />
+      </button>
+      {showConfig && (
+        <div className="flex mb-10 justify-center">
+          <div
+            className="select-none cursor-pointer mr-5 last:mr-0"
+            onClick={() => {
+              config.set('enterKeyIsSend', !config.enterKeyIsSend);
+            }}
+          >
+            Enter key is send: <strong>{config.enterKeyIsSend ? 'yes' : 'no'}</strong>
+          </div>
 
-      <div
-        className="select-none cursor-pointer mr-5 last:mr-0"
-        onClick={() => {
-          config.set('sendAfter5s', !config.sendAfter5s);
-        }}
-      >
-        Simulate promises(5s): <strong>{config.sendAfter5s ? 'yes' : 'no'}</strong>
-      </div>
-    </div>
+          <div
+            className="select-none cursor-pointer mr-5 last:mr-0"
+            onClick={() => {
+              config.set('sendAfter5s', !config.sendAfter5s);
+            }}
+          >
+            Simulate promises(5s): <strong>{config.sendAfter5s ? 'yes' : 'no'}</strong>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
